539db9c86f559f77609be9d7cd8536cc9236b15a
Ayushman Math <65216063+ayushmath07@users.noreply.github.com>
2026-07-27 01:00:39 +0530

    Time: 3 ms (86.33%), Space: 43 MB (86.05%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stk = new Stack<>();
+        for(int i =0;i<s.length();i++){
+            char c = s.charAt(i);
+            if(c=='(' || c=='[' || c=='{'){
+                stk.push(c);
+            }
+            else{
+                if(stk.isEmpty()) return false;
+                else if(c==')' && stk.peek()=='(' || c=='}' && stk.peek()=='{' || c==']' && stk.peek()=='['){
+                    stk.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return stk.isEmpty()?true:false;
+    }
+}
